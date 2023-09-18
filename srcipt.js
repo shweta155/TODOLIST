938bde46cb71570a3eaa54368e23919f21f979dd
shweta Ladne <112686792+shweta155@users.noreply.github.com>
2023-09-18 20:35:58 +0530

    Add files via upload

---
+++ srcipt.js
@@ -0,0 +1,96 @@
+// Load tasks from local storage
+const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+// Display tasks
+function displayTasks() {
+  const taskList = document.getElementById('taskList');
+  taskList.innerHTML = '';
+
+  tasks.forEach((task, index) => {
+    const card = document.createElement('div');
+    card.className = 'card';
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.className = 'task-checkbox';
+    checkbox.checked = task.completed || false;
+    checkbox.addEventListener('change', () => toggleTaskStatus(index));
+
+    const taskText = document.createElement('span');
+    taskText.textContent = task.name;
+
+    const dueDate = document.createElement('span');
+    dueDate.textContent = task.dueDate;
+    dueDate.className = 'due-date';
+
+    const deleteButton = document.createElement('button');
+    deleteButton.className = 'delete-button';
+    deleteButton.innerHTML = '<i class="delete-button-icon">&#10006;</i>';
+    deleteButton.addEventListener('click', () => deleteTask(index));
+
+    card.appendChild(checkbox);
+    card.appendChild(taskText);
+    card.appendChild(dueDate);
+    card.appendChild(deleteButton);
+    taskList.appendChild(card);
+
+    // Check if task is overdue
+    const isOverdue = isTaskOverdue(task.dueDate);
+    const isChecked = checkbox.checked;
+
+    if (isOverdue && !isChecked) {
+      card.classList.add('overdue');
+    } else if (!isChecked) {
+      card.classList.add('not-completed');
+    } else {
+      card.classList.add('completed');
+    }
+  });
+}
+
+
+// Add a new task
+function addTask() {
+  const taskInput = document.getElementById('taskInput');
+  const dueDateInput = document.getElementById('dueDateInput');
+  const taskName = taskInput.value.trim();
+  const dueDate = dueDateInput.value;
+
+  if (taskName !== '') {
+    const newTask = {
+      name: taskName,
+      dueDate: dueDate,
+      completed: false
+    };
+
+    tasks.push(newTask);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    taskInput.value = '';
+    dueDateInput.value = '';
+    displayTasks();
+  }
+}
+
+// Delete a task
+function deleteTask(index) {
+  tasks.splice(index, 1);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  displayTasks();
+}
+
+// Toggle task status
+function toggleTaskStatus(index) {
+  tasks[index].completed = !tasks[index].completed;
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  displayTasks();
+}
+
+
+// Check if task is overdue
+function isTaskOverdue(dueDate) {
+  const now = new Date().setHours(0, 0, 0, 0);
+  const taskDueDate = new Date(dueDate).setHours(0, 0, 0, 0);
+  return now > taskDueDate;
+}
+
+displayTasks();
